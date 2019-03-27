f20063b2afbd416b9bd5fd9c41d37c3041417366
LewisLXC <1853199517@qq.com>
2019-03-27 20:12:41 +0800

    创建服务注册中心

---
+++ cloud-eureka/src/main/java/com.spring.cloud.eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author
+ * Eureka 是一个服务注册和发现模块
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
