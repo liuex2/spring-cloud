72ca14670daeb29d49dcf30505647bf9d9d39aef
LewisLXC <1853199517@qq.com>
2019-03-28 10:20:38 +0800

    服务链路追踪相关

---
+++ cloud-zipkin/src/main/java/com/spring/cloud/zipkin/ZipkinApplication.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @Author
+ *
+ * 服务链路追踪
+ *
+ * 通过 @EnableZipkinServer 注解开启 Zipkin Server 功能
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipkinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+}
---
+++ cloud-zipkin/src/test/java/com.spring.cloud.zipkin/Test.java
@@ -0,0 +1,17 @@
+package com.spring.cloud.zipkin;
+
+/**
+ * @Author
+ *
+ * 测试用例
+ *
+ */
+public class Test {
+
+    /**
+     * 启动全部项目，打开浏览器访问：http://localhost:9411/ 会出现以下界面：
+     *
+     * 点击 Find a trace，可以看到具体服务相互调用的数据
+     */
+    public void test(){}
+}
