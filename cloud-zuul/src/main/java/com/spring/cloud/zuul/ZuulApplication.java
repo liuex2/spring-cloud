9c2375670bc963ab6dcc01255c99d16bfa214e25
LewisLXC <1853199517@qq.com>
2019-03-28 10:22:56 +0800

    配置api网关

---
+++ cloud-zuul/src/main/java/com/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Author
+ *使用路由网关统一访问接口
+ *
+ * Zuul 的主要功能是路由转发和过滤器。
+ * 路由功能是微服务的一部分，比如 /api/user 转发到到 User 服务，/api/shop 转发到到 Shop 服务。
+ * Zuul 默认和 Ribbon 结合实现了负载均衡的功能。
+ *
+ * 增加 @EnableZuulProxy 注解开启 Zuul 功能
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
